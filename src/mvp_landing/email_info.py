58ee181517590cb2176ea34375798bef8a037ee5
Nox <hrichardson83@gmail.com>
2014-03-26 19:11:42 -0400

    Update email_info.py

--- src/mvp_landing/email_info.py
+++ src/mvp_landing/email_info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'hrichardson@gmail.com'
-EMAIL_HOST_PASSWORD = "Indiglo6"
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ""
 EMAIL_PORT = 587
